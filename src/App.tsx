239e4fa0ea75741e798ea1477a43040fb4dcd1d7
Voitsekhovich <leshavoitsekhovich@gmail.com>
2024-06-06 21:48:33 +0300

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { HW2 } from './HW/2/HW2';
 import { HW3 } from './HW/3/HW3';
 import { HW4 } from './HW/4/HW4';
 
-function App() {
+function App(
+) {
   return (
     <div className="App">
-      {/*<HW1 />*/}
+      <HW1 />
+
+
       {/*<HW2 />*/}
       {/*<HW3 />*/}
       {/*<HW4 />*/}
